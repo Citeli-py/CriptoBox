5dfbfba19ce82a82b5d2eb143f8d101b2e5d6327
Matheus Citeli <matheus.citeli2001@gmail.com>
2025-02-21 19:20:20 -0300

    :recycle: Refactor: Update para CommomJS

--- src/teste.js
+++ src/teste.js
@@ -1,23 +1,23 @@
-import FileController from './controller/FileController.js';
-import CipherUtils from './utils/CipherUtils.js';
-import KeyController from './controller/KeyController.js';
-import EncryptedFileHandler from './utils/EncryptedFileHandler.js';
+const FileController = require('./controller/FileController.js');
+const CipherUtils = require('./utils/CipherUtils.js');
+const KeyController = require('./controller/KeyController.js');
+const EncryptedFileHandler = require('./utils/EncryptedFileHandler.js');
 
 // Gerando a chave de criptografia a partir da senha
-const key = CipherUtils.generateKey("senha_fort");
+const key = CipherUtils.generateKey("senha_forte");
 // FileController.create("arquivo_1", key);
 
 // Lendo a chave armazenada
 console.log(KeyController.read(key));
 
 // Criando um arquivo criptografado
-// FileController.create("arquivo_1", key);
+FileController.create("arquivo_2", key);
 
 // Adicionando um novo token à KeyController
 // KeyController.add(key, "Alou isso é um teste", key);
 
 // Escrevendo no arquivo criptografado
-// FileController.write('arquivo_1', "Olá, sou um arquivo importante,\nEstou testando para ver se funciona o meu arquivo criptografado\nSenha: 12345", key);
+FileController.write('arquivo_2', "Olá, sou o segundo arquivo importante,\nEstou testando para ver se funciona o meu arquivo criptografado\nSenha: 12345", key);
 
 // Lendo o conteúdo criptografado do arquivo
-console.log(FileController.read("arquivo_1", key));
+console.log(FileController.read("arquivo_2", key));
