4a66a3755aef081819163e3ace291cb45bcb35b7
Citeli-py <matheus.citeli2001@gmail.com>
2025-03-06 09:49:09 -0300

    :sparkles: FEATURE: implementação de IPC

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const KeyController = require('./src/controller/KeyController.js');
 const CipherUtils = require('./src/utils/CipherUtils.js');
+const FileController = require('./src/controller/FileController.js');
 
 let mainWindow;
 
@@ -18,8 +19,22 @@ app.whenReady().then(() => {
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 });
 
+ipcMain.handle('get-secretKey', async (event, password) => {
+    return CipherUtils.generateKey(password);
+})
+
 // IPC listener para obter as chaves
 ipcMain.handle('get-keys', async (event, secretKey) => {
-    const key = CipherUtils.generateKey("senha_forte");
-    return KeyController.read(key);
+    return KeyController.read(secretKey);
+});
+
+// IPC listener para abrir um arquivo
+ipcMain.handle('get-file', async (event, secretKey, file) => {
+    return FileController.read(file, secretKey);
+});
+
+// IPC listener para salvar um arquivo
+ipcMain.handle('save-file', async (event, secretKey, file, text) => {
+    FileController.write(file, text, secretKey);
+    console.log("Salvou")
 });
