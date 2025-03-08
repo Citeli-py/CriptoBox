e7d18dc722777dbc2d39a582c206b985209ead4b
Citeli-py <matheus.citeli2001@gmail.com>
2025-03-07 21:34:29 -0300

    Preparação para renameFile

--- preload.js
+++ preload.js
@@ -4,13 +4,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
     getSecretKey: (password)                => ipcRenderer.invoke('get-secretKey', password),
     getKeys: (secretKey)                    => ipcRenderer.invoke('get-keys', secretKey),
     getFile: (secretKey, file)              => ipcRenderer.invoke('get-file', secretKey, file),
-    saveFile: (secretKey, file, text)       => ipcRenderer.invoke('save-file', secretKey, file, text)
+    saveFile: (secretKey, file, text)       => ipcRenderer.invoke('save-file', secretKey, file, text),
+    //renameFile: (secretKey, file)           => ipcRenderer.invoke('rename-file', secretKey, file),
 });
-
-
-/**
-Salvar
-const info = JSON.stringify(quill.getContents().ops)
-quill.setContents(info)
-quill.setContents(JSON.parse(info))
- */
