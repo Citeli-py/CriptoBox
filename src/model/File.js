e403d2f0620327eddf2c556d8e480de2a500facc
Citeli-py <matheus.citeli2001@gmail.com>
2025-03-08 21:37:42 -0300

    Modelo de arquivo

---
+++ src/model/File.js
@@ -0,0 +1,12 @@
+
+
+class File {
+
+    constructor(filename, text, token){
+        this.filename = filename;
+        this.text = text;
+        this.token = token;
+    }
+};
+
+module.exports =  File;
